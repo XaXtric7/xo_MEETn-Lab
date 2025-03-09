0f61499222c1822ad8400a1b0d5b54c202ddfcbe
XaXtric7 <sarthakdharmik10227xix@gmail.com>
2025-03-09 11:10:30 +0530

    QUE_Organize_Directory

---
+++ V7/organize2.js
@@ -0,0 +1,40 @@
+// You have to write a Node.js program to clear clutter inside of a directory and organize the contents of that directory into different folders
+
+// for example, these files become:
+
+// 1. name.jpg
+// 2. name.png
+// 3. this.pdf
+// 4. sarthak.zip
+// 5. Rohan.zip
+// 6. cat.jpg
+// 7. harry.pdf
+
+// this:
+// jpg/name.jpg, jpg/cat.jpg
+// png/name.png
+// pdf/this.pdf pdf/sarthak.pdf
+// zip/sarthak.zip zip/Rohan.zip
+
+import fs from "fs/promises";
+import fsn from "fs";
+import path from "path";
+
+const basepath =
+  "D:\\Sarthak\\Academic Files\\Coding and Language Stuff\\CodeWithHarry\\Node JS(CWH)\\xo_Node.js\\V7";
+
+let files = await fs.readdir(basepath);
+
+for (const item of files) {
+  console.log("running for ", item);
+  let ext = item.split(".")[item.split(".").length - 1];
+  if (ext != "js" && ext != "json" && item.split(".").length > 1) {
+    if (fsn.existsSync(path.join(basepath, ext))) {
+      // Move the file to this directory if its not a js or json file
+      fs.rename(path.join(basepath, item), path.join(basepath, ext, item));
+    } else {
+      fs.mkdir(ext);
+      fs.rename(path.join(basepath, item), path.join(basepath, ext, item));
+    }
+  }
+}
